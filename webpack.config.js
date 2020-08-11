b698e4f4aaba8440462b609b5c55ae013ef2fb9d
Han Huang <huanghan@corp-ci.com>
2020-08-11 10:55:07 +0800

    feat: 优化打包大小

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,60 @@
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode: 'none',
-    entry: {
-        map: './src/map.tsx',
-        background: './src/background.ts',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ['.ts', '.tsx', '.js', '.json'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                        },
-                    },
-                    {
-                        loader: 'less-loader',
-                    },
-                ],
+  mode: 'production',
+  entry: {
+    map: './src/map.tsx',
+    background: './src/background.ts',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
             },
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            {test: /\.tsx?$/, loader: 'ts-loader', exclude: '/node_modules/'},
+          },
+          {
+            loader: 'less-loader',
+          },
         ],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                {from: './manifest.json'},
-            ],
-        }),
+      },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {test: /\.tsx?$/, loader: 'ts-loader', exclude: '/node_modules/'},
     ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: './manifest.json'},
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 }
